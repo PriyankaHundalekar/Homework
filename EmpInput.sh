cd64525488cf0310b6533f3204f8bb2ba0d8a34a
PriyankaHundalekar <priyanka.hundalekar6264@gmail.com>
2020-05-19 00:03:02 +0530

    EmpInput is added

--- EmpInput.sh
+++ EmpInput.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -x
 
+declare -A result
 read -p "Enter first Input : " a
 read -p "Enter second Input : " b
 read -p "Enter third Input : " c
 
+d=$(($a+$b*$c))
+result=$d
+echo $result
