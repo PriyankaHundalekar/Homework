c0a32e6c8d7777d9d32638fca0c0edf08b46dd8b
PriyankaHundalekar <priyanka.hundalekar6264@gmail.com>
2020-05-19 10:50:46 +0530

    EmpInput is added

--- EmpInput.sh
+++ EmpInput.sh
@@ -8,9 +8,12 @@ read -p "Enter third Input : " c
 d=$(($a+$b*$c))
 e=$(($a*$b+$c))
 f=$(($c+$a/$b))
+g=$(($a%$b+$c))
 result1=$d
 result2=$e
 result3=$f
+result4=$g
 echo $result1
 echo $result2
 echo $result3
+echo $result4
